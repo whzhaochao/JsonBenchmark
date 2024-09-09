106b977160c490f9f324ff37daec53aa3c3f16d2
zhaochao <whzhaochao@163.com>
2024-09-09 19:26:45 +0800

    INIT

--- src/main/java/com/json/MediumJsonDerializer.java
+++ src/main/java/com/json/MediumJsonDerializer.java
@@ -1,11 +1,12 @@
 package com.json;
 
-import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.json.domain.User;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.*;
@@ -13,15 +14,15 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @State(Scope.Thread)
-public class MediaJsonSerialize {
+public class MediumJsonDerializer {
 
-    private List<User> users;
+    private String json;
 
     private ObjectMapper mapper;
 
@@ -29,59 +30,62 @@ public class MediaJsonSerialize {
 
     @Setup
     public void setup() {
-        users = new ArrayList<>();
+        List<User> users = new ArrayList<>();
         IntStream.range(1, 20).forEach(x -> {
             User user = new User();
             user.setId(1L);
-            user.setName(RandomUtil.randomString("公众号:赵侠客",100));
+            user.setName("赵侠客");
             user.setAge(29);
             user.setSex("男");
-            user.setTrueName(RandomUtil.randomString("公众号:赵侠客",100));
-            user.setCreateTime(new Date());
+            user.setTrueName("公众号");
             users.add(user);
         });
+        json = JSON.toJSONString(users);
         mapper = new ObjectMapper();
         gson = new Gson();
     }
 
     @TearDown
     public void tearDown() {
-        users = null;
+        json = null;
         mapper = null;
         gson = null;
     }
 
     @Benchmark
     public void testFastJson() {
-        String json = JSON.toJSONString(users);
+        List<User> users = JSON.parseArray(json, User.class);
     }
 
     @Benchmark
     public void testFast2Json() {
-        String json = com.alibaba.fastjson2.JSON.toJSONString(users);
+        List<User> user = com.alibaba.fastjson2.JSON.parseArray(json, User.class);
     }
+
 
     @Benchmark
     public void testHutoolJson() {
-        String json = JSONUtil.toJsonStr(users);
+        List<User> users = JSONUtil.toList(json, User.class);
     }
-
 
     @Benchmark
     public void testJackson() throws JsonProcessingException {
-        String json = mapper.writeValueAsString(users);
+        JavaType javaType = mapper.getTypeFactory().constructCollectionType(List.class, User.class);
+        List<User> users = mapper.readValue(json, javaType);
     }
 
     @Benchmark
     public void testGson() {
-        String json = gson.toJson(users);
+        Type type = new TypeToken<List<User>>() {
+        }.getType();
+        List<User> users = gson.fromJson(json, type);
     }
 
 
     @Test
     public void testBenchmark() throws Exception {
         Options options = new OptionsBuilder()
-                .include(MediaJsonSerialize.class.getSimpleName())
+                .include(MediumJsonDerializer.class.getSimpleName())
                 .forks(1)
                 .threads(1)
                 .warmupIterations(1)
--- src/main/java/com/json/MediumJsonSerialize.java
+++ src/main/java/com/json/MediumJsonSerialize.java
@@ -1,12 +1,11 @@
 package com.json;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.json.domain.User;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.*;
@@ -14,15 +13,15 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @State(Scope.Thread)
-public class MediaJsonDerializer {
+public class MediumJsonSerialize {
 
-    private String json;
+    private List<User> users;
 
     private ObjectMapper mapper;
 
@@ -30,62 +29,59 @@ public class MediaJsonDerializer {
 
     @Setup
     public void setup() {
-        List<User> users = new ArrayList<>();
+        users = new ArrayList<>();
         IntStream.range(1, 20).forEach(x -> {
             User user = new User();
             user.setId(1L);
-            user.setName("赵侠客");
+            user.setName(RandomUtil.randomString("公众号:赵侠客",100));
             user.setAge(29);
             user.setSex("男");
-            user.setTrueName("公众号");
+            user.setTrueName(RandomUtil.randomString("公众号:赵侠客",100));
+            user.setCreateTime(new Date());
             users.add(user);
         });
-        json = JSON.toJSONString(users);
         mapper = new ObjectMapper();
         gson = new Gson();
     }
 
     @TearDown
     public void tearDown() {
-        json = null;
+        users = null;
         mapper = null;
         gson = null;
     }
 
     @Benchmark
     public void testFastJson() {
-        List<User> users = JSON.parseArray(json, User.class);
+        String json = JSON.toJSONString(users);
     }
 
     @Benchmark
     public void testFast2Json() {
-        List<User> user = com.alibaba.fastjson2.JSON.parseArray(json, User.class);
+        String json = com.alibaba.fastjson2.JSON.toJSONString(users);
     }
-
 
     @Benchmark
     public void testHutoolJson() {
-        List<User> users = JSONUtil.toList(json, User.class);
+        String json = JSONUtil.toJsonStr(users);
     }
+
 
     @Benchmark
     public void testJackson() throws JsonProcessingException {
-        JavaType javaType = mapper.getTypeFactory().constructCollectionType(List.class, User.class);
-        List<User> users = mapper.readValue(json, javaType);
+        String json = mapper.writeValueAsString(users);
     }
 
     @Benchmark
     public void testGson() {
-        Type type = new TypeToken<List<User>>() {
-        }.getType();
-        List<User> users = gson.fromJson(json, type);
+        String json = gson.toJson(users);
     }
 
 
     @Test
     public void testBenchmark() throws Exception {
         Options options = new OptionsBuilder()
-                .include(MediaJsonDerializer.class.getSimpleName())
+                .include(MediumJsonSerialize.class.getSimpleName())
                 .forks(1)
                 .threads(1)
                 .warmupIterations(1)
